e18c7562c0e81077339ca63dabe3cfa7370b367d
JoshThomson7 <josh@fl1.digital>
2024-03-04 13:53:19 +0000

    New coco hand

--- modules/advanced-video-banners/templates/avb_image.php
+++ modules/advanced-video-banners/templates/avb_image.php
@@ -10,5 +10,5 @@ $avb_banner_image = new AVB_Banner_Image($banner_data);
 ?>
 <div class="avb-banner__medium <?php echo $avb_banner_image->classes(); ?>"
 	<?php echo $avb_banner_image->image() ? 'style="background-image:url('.$avb_banner_image->image().');"' : ''; ?> role="img" alt="<?php echo $avb_banner_image->image('alt'); ?>">
-	<img src="<?php echo esc_url(get_stylesheet_directory_uri().'/img/coconut-hand.png'); ?>" alt="<?php bloginfo('name'); ?>"/>
+	<img src="<?php echo esc_url(get_stylesheet_directory_uri().'/img/coco-hand-iso-min.png'); ?>" alt="<?php bloginfo('name'); ?>"/>
 </div>
